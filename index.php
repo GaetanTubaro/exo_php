2881c34a88b53fe7fc0d61452202f7f71018be1b
Gaetan_Tubaro <tubaro.gaetan@gmail.com>
2022-01-26 16:23:12 +0100

    exo10

--- index.php
+++ index.php
@@ -1,20 +1,9 @@
 <?php
-include 'variables.php';
-include 'function.php';
+require_once 'includes/variables.php';
+require_once 'includes/function.php';
+require_once 'includes/header.php'
 ?>
 
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
     <table>
         <tr>
             <th>Index</th>
@@ -29,5 +18,7 @@ include 'function.php';
         }
         ?>
     </table>
-</body>
-</html>
+
+<?php
+    require_once 'includes/footer.php'
+?>
