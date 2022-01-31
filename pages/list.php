0ab4a059d1ff0045bd7604a94eca0ace2982666f
Gaetan_Tubaro <tubaro.gaetan@gmail.com>
2022-01-31 13:27:18 +0100

    exo 21

--- pages/list.php
+++ pages/list.php
@@ -1,16 +1,3 @@
-<?php
-// if (isset($_GET['key'])) {
-//     if (!isset($_SESSION['cart'])) {
-//         $_SESSION['cart'] = [];
-//     }
-//     if (isset($_SESSION['cart'][$_GET['key']])) {
-//         $_SESSION['cart'][$_GET['key']]++;
-//     } else {
-//         $_SESSION['cart'][$_GET['key']] = 1;
-//     }
-// }
-?>
-
 <table>
     <tr>
         <th>Nom</th>
